ea5be40390ff449a5c6f73f63dca41aa098d707d
Kindra Kinney <kindrakinney@gmail.com>
2019-12-18 16:44:28 -0800

    adding js files

--- script.js
+++ script.js
@@ -1,95 +1,96 @@
-//DOM Elements
-var lengthElement = document.querySelector('#passlength');
-var uppercaseElement = document.querySelector('#uppercase');
-var lowercaseElement = document.querySelector('#lowercase');
-var numbersElement = document.querySelector('#numbers');
-var symbolsElement = document.querySelector('#symbols');
-var outputElement = document.querySelector('#passwordBox');
-var generateElement = document.querySelector('#generateButton');
-var clipboardElement = document.querySelector('#clipboardButton');
-
-//Randomizing fuctions defined
-function randomLowercase() {
-    var lowers = 'abcdefghijklmnopqrstuvwxyz'
-
-    if (lowercaseElement.checked == true) {
-        return lowers[Math.floor(Math.random() * lowers.length)]
-    }
-    else {
-        return ''
-    }
+const userPassEl = document.querySelector('#userPass');
+const lengthEl = document.querySelector('#passwordLength');
+const lowercaseEl = document.querySelector('#lowercase');
+const uppercaseEl = document.querySelector('#uppercase');
+const numbersEl = document.querySelector('#numbers');
+const symbolsEl = document.querySelector('#symbols');
+    
+// functions generate random with return strings of browser characters sets
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
-
-function randomUppercase() {
-    var uppers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
-    if (uppercaseElement.checked == true) {
-        return uppers[Math.floor(Math.random() * uppers.length)]
-    }
-
-    else {
-        return ''
-    }
+        
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
-
-function randomNumber() {
-    var numbers = '0123456789'
-
-    if (numbersElement.checked == true) {
-        return numbers[Math.floor(Math.random() * numbers.length)];
-    }
-
-    else {
-        return ''
-    }
-
+        
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);        
 }
-
-function randomSymbol() {
-    var symbols = '!@#$%^&*(){}[]=<>/'
-    if (symbolsElement.checked == true) {
-        return symbols[Math.floor(Math.random() * symbols.length)];
-    }
-    else {
-        return ''
-    }
+        
+function getRandomSymbol() {
+    const symbols = ' !"#$%&()*+,-./:;<=>?@[]^_`{|}~'
+    return symbols[Math.floor(Math.random() * symbols.length)];
 }
+    
+//set array with keys to each getRandom functions
+    const randomChar = {
+        lower: getRandomLower,
+        upper: getRandomUpper,
+        numbers: getRandomNumber,
+        symbols: getRandomSymbol,
+    };
+  
+generateButton.addEventListener('click', () => {
+        //set password length
+        const passwordLength = +lengthEl.value;
+        //assign 'checked' values
+        const checkedLower = lowercaseEl.checked;
+	    const checkedUpper = uppercaseEl.checked;
+	    const checkedNumbers = numbersEl.checked;
+        const checkedSymbols = symbolsEl.checked;
+        //get the return value or password from the function generatePassword
+        userPassEl.innerText = generatePassword(passwordLength, checkedLower, checkedUpper, checkedNumbers, checkedSymbols);
+    });
+    
+        
+        function generatePassword(passwordLength, lower, upper, numbers, symbols) { 
+            
+            let password = '';
+            // set types for random pull loop
+            const typesChar = lower + upper + numbers + symbols;
+            //set up array for checked or not checked filter
+            const typesCharArr = [{lower}, {upper}, {numbers}, {symbols}].filter(
+                //filter true boolean from false
+                item => Object.values(item)[0]);
+                //check if no types were selected
+                if(typesChar === 0) {
+                    alert("Must select at least one type of character!")
+                }
+                
+                //loop through the types and pull random character from each type
+                for(let i = 0; i < passwordLength; i += typesChar){
+                    typesCharArr.forEach(type => {
+                        const charKey = Object.keys(type)[0];
+                        console.log("charkey :" +charKey)
+                        password += randomChar[charKey]();
+                        
+                    });
+                }
+                    
+                    const userPassword = password
+                    
+                    return userPassword;
+                    //console.log(userPassword)
+            
+            }  
+            
 
-//Generate Password Functions
-var newPassword
-
-function generatePassword() {
-    var newPassword = ""
-    var length = lengthElement.value
-    for (var i = 0; i < length; i++) {
-        newPassword += randomLowercase()
-        newPassword += randomUppercase()
-        newPassword += randomNumber()
-        newPassword += randomSymbol()
-    }
-
-    newPassword = newPassword.slice(0, length)
-    outputElement.value = newPassword
-}
-
-function clear(outputElement) {
-    outputElement.value = "";
-}
-
-//Generate Button Click Function
-generateElement.addEventListener("click", function () {
-    clear(outputElement);
-    generatePassword();
-})
-
-//Copy to Clipboard
-function copy() {
-    outputElement.select();
-    document.execCommand("copy");
-    alert("Your new password has been copied to the clipboard.")
-}
-
-// //Copy Button Click Function
-clipboardElement.addEventListener("click", function () {
-    copy()
-})
+    //function that copies passwor to clipboard after click copy button
+copyButton.addEventListener('click', () => {
+    
+    const textarea = document.createElement("textarea");
+    const copiedPass = userPassEl.innerText;
+    
+    textarea.value = copiedPass;
+    
+    document.body.appendChild(textarea);
+    
+    textarea.select();
+    
+    document.execCommand('copy');
+    
+    textarea.remove();
+    
+    alert("Password copied");
+});
